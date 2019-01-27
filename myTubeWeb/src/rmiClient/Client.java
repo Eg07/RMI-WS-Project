312e2ca4fb8cf2ee3a1fdd2aad6e6aa1a6cf5a9b
ea07 <elieghaith@gmail.com>
2019-01-28 00:16:33 +0100

    finish rmi client

--- myTubeWeb/src/rmiClient/Client.java
+++ myTubeWeb/src/rmiClient/Client.java
@@ -1,12 +1,6 @@
 package rmiClient;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -17,7 +11,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-import javax.ws.rs.core.MediaType;
 
 import model.Content;
 import rmiService.ContentService;
@@ -46,7 +39,7 @@ public class Client {
 
 		char c = 'c';
 		System.out.println("\t\t\t\t\t\t-----------------------------------------");
-		System.out.println("\t\t\t\t\t\t| Welcome to My Tube Web Service        |");
+		System.out.println("\t\t\t\t\t\t| Welcome to My Tube RMI Application    |");
 		System.out.println("\t\t\t\t\t\t-----------------------------------------");
 		opt();
 		while (true) {
@@ -217,186 +210,39 @@ public class Client {
 			System.out.println(c.toString());
 	}
 
-	private static void postNewContent(Content newContent) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/post");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-
-			String jsonFromPojo = newContent.getJson();
-
-			OutputStream os = conn.getOutputStream();
-			os.write(jsonFromPojo.getBytes());
-			os.flush();
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output;
-			while ((output = br.readLine()) != null) {
-				System.out.println("\nClient POST. Response: " + output);
-			}
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void postNewContent(Content newContent) throws RemoteException {
+		contentService.insertConent(newContent);
 	}
 
-	private static void delete(String id) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/delete?id=" + id);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_JSON);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output;
-			while ((output = br.readLine()) != null) {
-				System.out.println("\nClient Search. Response: " + output);
-			}
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void delete(String id) throws NumberFormatException, RemoteException {
+		contentService.deleteContent(Integer.parseInt(id));
 	}
 
-	private static void search(String title) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/search?title=" + title);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_JSON);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output = br.readLine();
-			ArrayList<String> list = new ArrayList<>();
-
-			for (String str : output.split("},"))
-				list.add(str + "}");
-
-			for (String s : list)
-				System.out.println("\t " + s);
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void search(String title) throws RemoteException {
+		contentService.search(title);
 	}
 
-	private static void update(String title, String topic, String id) {
-		try {
-			URL url = new URL(
-					"http://localhost:8080/myTubeWeb/rest/update?title=" + title + "&topic=" + topic + "&id=" + id);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_JSON);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output;
-			while ((output = br.readLine()) != null) {
-				System.out.println("\nClient Search. Response: " + output);
-			}
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void update(String title, String topic, String id) throws NumberFormatException, RemoteException {
+		Content temp = new Content();
+		temp.setTitle(title);
+		temp.setTopic(topic);
+		contentService.updateContent(temp, Integer.parseInt(id));
 	}
 
-	private static void getTopic(String topic) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/search?title=" + topic);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_JSON);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output = br.readLine();
-			ArrayList<String> list = new ArrayList<>();
-
-			for (String str : output.split("},"))
-				list.add(str + "}");
-
-			for (String s : list)
-				System.out.println("\t " + s);
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void getTopic(String topic) throws RemoteException {
+		List<Content> temp = new ArrayList<>();
+		temp = contentService.getByTopic(topic);
+		
+		for (Content c : temp)
+			System.out.println(c.toString());
 	}
 
-	private static void downloadId(String id) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/getId?id=" + id);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_OCTET_STREAM);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output;
-			while ((output = br.readLine()) != null) {
-				System.out.println("\nClient Download by ID . Response: " + output);
-			}
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void downloadId(String id) throws NumberFormatException, RemoteException {
+		contentService.getContentById(Integer.parseInt(id));
 	}
 
-	private static void downloadName(String name) {
-		try {
-			URL url = new URL("http://localhost:8080/myTubeWeb/rest/getName?name=" + name);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestProperty("Accept", MediaType.APPLICATION_OCTET_STREAM);
-			conn.setRequestMethod("GET");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String output;
-			while ((output = br.readLine()) != null) {
-				System.out.println("\nClient Download by Name . Response: " + output);
-			}
-			conn.disconnect();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	private static void downloadName(String name) throws RemoteException {
+		contentService.getContentByName(name);
 	}
 
 	private static void opt() {
